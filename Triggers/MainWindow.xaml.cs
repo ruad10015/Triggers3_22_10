713d8b9d8d47e64eaabd713e0f356ca7c6b3eb51
Dunya_sy83 <Dunya_sy83@STHQ012D-03>
2023-11-14 10:59:16 +0400

    Add project files.

---
+++ Triggers/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Triggers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; OnPropertyChanged(); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Text = "no";
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void redColorCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Text = (Text == "yes") ? "no" : "yes";
+        }
+    }
+}
